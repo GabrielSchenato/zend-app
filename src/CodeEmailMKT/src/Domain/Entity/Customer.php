2a60405f66d89e5bc11436f6057a51b598c3de86
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-05-29 16:17:29 -0300

    Quarto commit, mapeando Customer com YAML e instalado YAML

---
+++ src/CodeEmailMKT/src/Domain/Entity/Customer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace CodeEmailMKT\Domain\Entity;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Customer
+ *
+ * @author gabriel
+ */
+class Customer 
+{
+    private $id;
+    
+    private $name;
+    
+    private $email;
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getName() {
+        return $this->name;
+    }
+
+    function getEmail() {
+        return $this->email;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setName($name) {
+        $this->name = $name;
+    }
+
+    function setEmail($email) {
+        $this->email = $email;
+    }
+}
