abb32604937b78bf9661fa069a812d79ac45626d
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-06-01 11:03:49 -0300

    Pegando twig enviroment

---
+++ src/CodeEmailMKT/src/Infrastructure/View/Twig/TwigRenderer.php
@@ -0,0 +1,18 @@
+<?php
+namespace CodeEmailMKT\Infrastructure\View\Twig;
+
+use Twig_Environment as TwigEnvironment;
+use \Zend\Expressive\Twig\TwigRenderer as ZendTwigRenderer;
+/**
+ * Template implementation bridging twig/twig
+ */
+class TwigRenderer extends ZendTwigRenderer
+{
+    /**
+     * @return TwigEnvironment
+     */
+    public function getTemplate()
+    {
+        return $this->template;
+    }
+}
